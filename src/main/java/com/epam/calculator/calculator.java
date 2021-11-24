b6f34fab39e33bc5b62ff55fc9cfb80f884e9d5e
Helen Parakhnevich <helen.parakhnevich@gmail.com>
2021-11-24 09:58:41 -0500

    initial commit

--- src/main/java/com/epam/calculator/calculator.java
+++ src/main/java/com/epam/calculator/calculator.java
@@ -4,4 +4,20 @@ public class Calculator {
     public static int Add(int first, int second) {
         return first + second;
     }
+
+    public static int Subtraction(int first, int second) {
+        return first - second;
+    }
+
+    public static int Multiplication(int first, int second) {
+        return first * second;
+    }
+
+    public static int Division(int first, int second) {
+        try {
+         return first / second;
+        } catch (ArithmeticException e) {
+            return 0;
+        }
+    }
 }
