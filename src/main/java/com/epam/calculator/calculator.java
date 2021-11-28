42ef1cfd917271081d50267d69cc949429f369c8
Helen Parakhnevich <helen.parakhnevich@gmail.com>
2021-11-28 18:49:49 -0500

    final to check commit

--- src/main/java/com/epam/calculator/calculator.java
+++ src/main/java/com/epam/calculator/calculator.java
@@ -3,23 +3,48 @@ package com.epam.calculator;
 import java.util.List;
 
 public class Calculator {
-    public static int Add(int first, int second) {
+    public static int add(int first, int second) {
         return first + second;
     }
 
-    public static int Subtraction(int first, int second) {
+    public static int subtract(int first, int second) {
         return first - second;
     }
 
-    public static int Multiplication(int first, int second) {
+    public static int multiply(int first, int second) {
         return first * second;
     }
 
-    public static int Division(int first, int second) {
+    public static int divide(int first, int second) {
         try {
          return first / second;
         } catch (ArithmeticException e) {
-            return 0;
+            throw new ArithmeticException("Division by 0");
         }
     }
+
+    public static double extractSquareRoot(double argument) {
+        double result =0.0;
+
+        result = Math.sqrt(argument);
+        if (Double.isNaN(result)) {
+            throw new ArithmeticException("The square root of a negative number does not exist");
+        }
+        else return result;
+    }
+
+    public static double extractRoot(double argument, double index) {
+        double result =0.0;
+
+        result = Math.pow(argument, 1/index);
+        if (Double.isNaN(result) || Double.isInfinite(result)) {
+            throw new ArithmeticException("Specified values cannot be used to extracting root");
+        }
+        else return result;
+    }
+
+    public static double raisePower(double base, double power) {
+        return Math.pow(base, power);
+    }
+
 }
--- src/test/java/com/epam/calculator/CalculatorTest.java
+++ src/test/java/com/epam/calculator/CalculatorTest.java
@@ -12,7 +12,7 @@ public class CalculatorTest {
         int secondArgument = 2;
 
         //when
-        int result = Calculator.Add(firstArgument, secondArgument);
+        int result = Calculator.add(firstArgument, secondArgument);
 
         //then
         Assert.assertEquals(3, result);
@@ -25,61 +25,151 @@ public class CalculatorTest {
         int secondArgument = -2;
 
         //when
-        int result = Calculator.Add(firstArgument, secondArgument);
+        int result = Calculator.add(firstArgument, secondArgument);
 
         //then
         Assert.assertEquals(-3, result);
     }
 
     @Test
-    public void testSubtractionWhenNumbersPositive() {
+    public void testSubtractWhenNumbersPositive() {
         //given
         int firstArgument = 1;
         int secondArgument = 2;
 
         //when
-        int result = Calculator.Subtraction(firstArgument, secondArgument);
+        int result = Calculator.subtract(firstArgument, secondArgument);
 
         //then
         Assert.assertEquals(-1, result);
     }
 
     @Test
-    public void testSubtractionWhenNumbersNegative() {
+    public void testSubtractWhenNumbersNegative() {
         //given
         int firstArgument = -1;
         int secondArgument = -2;
 
         //when
-        int result = Calculator.Subtraction(firstArgument, secondArgument);
+        int result = Calculator.subtract(firstArgument, secondArgument);
 
         //then
         Assert.assertEquals(1, result);
     }
 
     @Test
-    public void testMultiplication() {
+    public void testMultiply() {
         //given
         int firstArgument = -1;
         int secondArgument = -2;
 
         //when
-        int result = Calculator.Multiplication(firstArgument, secondArgument);
+        int result = Calculator.multiply(firstArgument, secondArgument);
 
         //then
         Assert.assertEquals(2, result);
     }
 
     @Test
-    public void testDivisionByZero() {
+    public void testDivideByZero() {
         //given
         int firstArgument = 1;
         int secondArgument = 0;
 
+        //then
+        Assert.assertThrows(ArithmeticException.class, () -> Calculator.divide(firstArgument, secondArgument));
+    }
+
+    @Test
+    public void testExtractSquareRootWhenNumberPositive() {
+        //given
+        double argument = 25;
+
         //when
-        int result = Calculator.Division(firstArgument, secondArgument);
+        double result = Calculator.extractSquareRoot(argument);
 
         //then
-        Assert.assertEquals(0, result);
+        Assert.assertEquals(5, result, 0.0);
+    }
+
+    @Test
+    public void testExtractSquareRootWhenNumberNegative() {
+        //given
+        double argument = -25;
+
+        //then
+        Assert.assertThrows(ArithmeticException.class, () -> Calculator.extractSquareRoot(argument));
+    }
+
+    @Test
+    public void testExtractRootWhenBasePositiveIndexPositive() {
+        //given
+        double argument = 25;
+        double index = 2;
+
+        //when
+        double result = Calculator.extractRoot(argument, index);
+
+        //then
+        Assert.assertEquals(5, result, 0.0);
+    }
+
+    @Test
+    public void testExtractRootWhenBaseNegativeIndexOdd() {
+        //given
+        double argument = -25;
+        double index = 2;
+
+        //then
+        Assert.assertThrows(ArithmeticException.class, () -> Calculator.extractRoot(argument, index));
+    }
+
+    @Test
+    public void testExtractRootWhenIndexZero() {
+        //given
+        double argument = 25;
+        double index = 0;
+
+        //then
+        Assert.assertThrows(ArithmeticException.class, () -> Calculator.extractRoot(argument, index));
+    }
+
+    @Test
+    public void testRaisePowerWhenBasePositivePowerPositive() {
+        //given
+        double base = 5;
+        double power = 3;
+
+        //when
+        double result = Calculator.raisePower(base, power);
+
+        //then
+        Assert.assertEquals(125, result, 0.0);
+    }
+
+    @Test
+    public void testRaisePowerWhenBasePositivePowerNegative() {
+        //given
+        double base = 5;
+        double power = -3;
+
+        //when
+        double result = Calculator.raisePower(base, power);
+
+        //then
+        Assert.assertEquals(0.008, result, 0.0);
+    }
+
+    @Test
+    public void testRaisePowerWhenBaseNegativePowerPositive() {
+        //given
+        double base = -5;
+        double power = 2;
+
+        //when
+        double result = Calculator.raisePower(base, power);
+
+        //then
+        Assert.assertEquals(25, result, 0.0);
     }
 }
